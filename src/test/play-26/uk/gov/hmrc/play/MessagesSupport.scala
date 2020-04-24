d27beabea52fb2057e757d3761e29ead707293fe
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-04-24 07:54:35 +0100

    PLATUI-436: Add support for fragments to formWithCSRF

--- src/test/play-26/uk/gov/hmrc/play/MessagesSupport.scala
+++ src/test/play-26/uk/gov/hmrc/play/MessagesSupport.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,8 +40,5 @@ trait MessagesSupport {
     ).get
   }
 
-  implicit val messagesRequest: MessagesRequest[_] =
-    new MessagesRequest(FakeRequest(), messagesApi)
-
-  implicit val messages: Messages = messagesRequest.messages
+  implicit val messages: Messages = new MessagesRequest(FakeRequest(), messagesApi).messages
 }
